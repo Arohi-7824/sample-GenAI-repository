4ddff95ce01da044307358de3cf937d9a99cb683
Arohi singh <arohisingh@Arohis-MacBook-Air.local>
2026-02-22 00:40:24 +0530

    script change

--- script.py
+++ script.py
@@ -2,8 +2,9 @@ print("Hello World")
 print("This is my first program")
 
 a=10
-name="Jacob Dennis"
-
+name="Arohi Singh"
+print(a)
+print(name)
 #This is a IF Condition
 if 10>5:
     print("10 is greater than 5!")
